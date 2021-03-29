4911804458c649e8e8d898a91d0f6a2bc4d77724
Karl5236543 <valentinvlad99@gmail.com>
2021-03-29 23:41:30 +0300

    fixed some bugs

--- __main__.py
+++ __main__.py
@@ -5,28 +5,25 @@ import socket
 
 def client():
     path_to_image = r'img.jpg'
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn.connect(('localhost', 8888))
     chunk=b''
     chunk_list = []
-    try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+        conn.connect(('localhost', 8888))
         while True:
             conn.send('next'.encode())
             chunk = conn.recv(2048)
+            print(len(chunk))
             if chunk:
-                index = int.from_bytes(chunk[:1], byteorder='big')
-                new_chunk = chunk[1:]
+                index, new_chunk = int.from_bytes(chunk[:1], byteorder='big'), chunk[1:]
                 chunk_list.append((index, new_chunk))
             else:
                 break
-    except ConnectionAbortedError:
-        conn.close()
-    sorted_chunks = sorted(chunk_list, key=lambda el: el[0])
-    data = b"".join([el[1] for el in sorted_chunks])    
-    
+
+    data = b"".join([el[1] for el in sorted(chunk_list)])    
     with open(path_to_image, 'wb') as f:
         f.write(data)
         f.flush()
+
     return path_to_image
 
 
